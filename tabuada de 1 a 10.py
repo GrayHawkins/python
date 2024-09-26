9dd5f315d0e9d29b3b9f57931d4f46d4347b44ed
Gabriel Oliveira <158515920+GrayHawkins@users.noreply.github.com>
2024-09-26 19:58:34 -0300

    Add files via upload

---
+++ tabuada de 1 a 10.py
@@ -0,0 +1,12 @@
+n1 = 1
+
+while n1 <= 10:
+    con1 = 0
+    print(f'tabuada do {n1}')
+    while con1 <= 10:
+        mult1 = n1*con1
+        print(f'{n1} x {con1} = {mult1}')
+        con1 += 1
+    n1 += 1
+    print ('\n')
+    
